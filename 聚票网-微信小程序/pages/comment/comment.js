211072c170532d6b4863f3f61f97b930fe6ee15a
lxjsghxs <1752950162@qq.com>
2020-07-02 17:04:38 +0800

    增加了评价、票品中的商家、订单点击到评价

--- 聚票网-微信小程序/pages/comment/comment.js
+++ 聚票网-微信小程序/pages/comment/comment.js
@@ -1,66 +1,43 @@
-// pages/comment/comment.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
+    evaluate_contant:['评价条目一'],
+    stars: [0, 1, 2, 3, 4],
+    normalSrc: '../../img/icon/no-star.png',
+    selectedSrc: '../../img/icon/full-star.png',
+    halfSrc: '../../img/icon/half-star.png',
+    score: 0,
+    scores: [0]
+  },
+
+  // 提交事件
+  submit_evaluate: function () {
+    console.log('评价得分' + this.data.scores*2)
+  },
+
+  //点击左边,半颗星
+  selectLeft: function (e) {
+    var score = e.currentTarget.dataset.score
+    if (this.data.score == 0.5 && e.currentTarget.dataset.score == 0.5) {
+      score = 0;
+    }
+
+    this.data.scores[e.currentTarget.dataset.idx] = score,
+      this.setData({
+        scores: this.data.scores,
+        score: score
+      })
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  //点击右边,整颗星
+  selectRight: function (e) {
+    var score = e.currentTarget.dataset.score
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.data.scores[e.currentTarget.dataset.idx] = score,
+      this.setData({
+        scores: this.data.scores,
+        score: score
+      })
   }
 })
